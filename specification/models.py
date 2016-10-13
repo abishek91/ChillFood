d1baa183d7ac861fed437f4bb1e73c1b29164cf7
delacruzpaulino <delacruzpaulino@gmail.com>
2016-10-13 16:06:58 -0400

    Fix: Models
    - Merged first and last name
    - Change age by birthdate
    - Add video link field
    - Change rating by tastiness and difficulty
    - Make required the description fields, for ingredients and instructions
    - Add field sort_by
    - Add list model

--- specification/models.py
+++ specification/models.py
@@ -4,49 +4,70 @@ from django.utils.timezone import datetime, now
 from django.contrib.postgres.fields import ArrayField
 
 class Person(models.Model):
-	first_name = models.CharField(max_length = 200, blank = True)
-	last_name = models.CharField(max_length = 200, blank = True)
-	email = models.EmailField(max_length = 200, blank = True)
-	age = models.IntegerField(blank = True)
+	name = models.CharField(max_length = 200)
+	email = models.EmailField(max_length = 200)
+	birthdate = models.DateField(null=True, blank=True, validators=[no_future_dates])
 	bio = models.CharField(max_length = 420, blank = True)
 	user = models.ForeignKey(User)
 	following = models.ManyToManyField("self", symmetrical=False, related_name='followers')
 	profile_pic = models.ImageField(upload_to='profile-pics', blank=True)
-	isActive = models.BooleanField(default=False)
-	Location = models.CharField(max_length = 200, blank = True)
+	is_active = models.BooleanField(default=False)
+	location = models.CharField(max_length = 200, blank = True)
 	def __unicode__(self):
 		return self.user
 
 class Recipe(models.Model):
-	title = models.CharField(max_length = 200, blank = True)
+	title = models.CharField(max_length = 200)
 	cook = models.ForeignKey(Person, on_delete=models.CASCADE)
-	likes = models.IntegerField(blank = True)
-	calories = models.CharField(max_length = 200, blank = True)
+	time = models.IntegerField()
+	calories = models.IntegerField(blank = True)
+	video_link = models.CharField(max_length = 200, blank = True)
 	date_time = models.DateTimeField(auto_now_add=True)
-
+	categories = ArrayField(models.CharField(max_length = 50, blank = True))
+	appliances = ArrayField(models.CharField(max_length = 50, blank = True))
+	cuisines = ArrayField(models.CharField(max_length = 50, blank = True))
+	
 class Ingredient(models.Model):
-	name = models.CharField(max_length = 200, blank = True)
+	recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+	name = models.CharField(max_length = 200)
 	quantity = models.CharField(max_length = 200, blank = True)
-	price = models.CharField(max_length = 200, blank = True)
+	price = models.IntegerField(blank = True)
 
 class Step(models.Model):
 	recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+	instruction = models.CharField(max_length = 200)
 	picture = models.ImageField(upload_to='steps', blank=True)
-	instruction = models.CharField(max_length = 200, blank = True)
-
+	
 class Comment(models.Model):
 	recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+	person = models.ForeignKey(Person, on_delete=models.CASCADE)
 	text = models.CharField(max_length = 200, blank = True)
-	upvotes = models.IntegerField(blank = True)
+	tastiness = models.IntegerField(blank = True)
+	difficulty = models.IntegerField(blank = True)
 	date_time = models.DateTimeField(auto_now_add=True)
+
+PREFERENCE_SORT_TYPE = (
+    (1, 'Difficulty'),
+    (2, 'Tastiness')
+)
 
 class Preferences(models.Model):
 	person = models.ForeignKey(Person, on_delete=models.CASCADE)
-	category = ArrayField(models.CharField(max_length = 50, blank = True))
+	categories = ArrayField(models.CharField(max_length = 50, blank = True))
 	appliances = ArrayField(models.CharField(max_length = 50, blank = True))
-	cuisine = ArrayField(models.CharField(max_length = 50, blank = True))
-	price_range = models.CharField(max_length = 50, blank = True)
-	videos = models.BooleanField()
-	difficulty = models.CharField(max_length = 200, blank = True)
+	cuisines = ArrayField(models.CharField(max_length = 50, blank = True))
+	price_min = models.IntegerField(blank = True)
+	price_max = models.IntegerField(blank = True)
+	has_video = models.BooleanField()
 	calories = models.IntegerField(blank = True)
-	rating = models.DecimalField(decimal_places = 1, max_digits = 2,blank = True)
+	sort_by = models.IntegerField(choices=PREFERENCE_SORT_TYPE, default = 1) 
+	
+LIST_TYPE = (
+    (1, 'Category'),
+    (2, 'Applicance'),
+    (3, 'Cuisine'),
+)
+
+class List(models.Model):
+	description = models.CharField(max_length = 200)
+	type = models.IntegerField(choices=LIST_TYPE)
