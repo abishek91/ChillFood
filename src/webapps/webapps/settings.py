7d7aefc4d01acd5ab847ae117ef77dc18ce09868
Abishek <abishek91@gmail.com>
2016-11-24 22:14:19 -0500

    Removed unnecessary import

--- src/webapps/webapps/settings.py
+++ src/webapps/webapps/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-import boto3
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR2 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
